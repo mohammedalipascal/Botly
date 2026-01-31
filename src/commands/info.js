46149e518ba192a71818a07e9aed208f331340a8
mohammedalipascal <110790215+mohammedalipascal@users.noreply.github.com>
2026-01-31 03:30:28 +0200

    Delete src directory

--- src/commands/info.js
+++
@@ -1,19 +0,0 @@
-module.exports = {
-    name: 'info',
-    description: 'معلومات عن البوت',
-    
-    async execute(sock, sender, args, msg) {
-        const info = `
-🤖 *معلومات البوت*
-
-📌 الإصدار: 1.0.0
-⚡ التقنية: Baileys (Node.js)
-🌐 الاستضافة: Clever Cloud
-💻 المطور: Your Name
-
-_بوت واتساب مبني بالكامل من الصفر_
-        `.trim();
-
-        await sock.sendMessage(sender, { text: info });
-    }
-};
